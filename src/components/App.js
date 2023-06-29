4430b957afbccd1359ffaf947b177e1fc4f96729
TarunVanshkar <tarunben33@gmail.com>
2023-06-29 19:45:03 +0530

    submitting code-Thu Jun 29 2023 19:44:57 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,34 @@
 
-import React from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(!username || !password){
+      setError("Both username and password are required.")
+    }
+    else{
+      setError("");
+    }
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="username">Username: </label>
+          <input type="text" id="username" onChange={(event) => setUsername(event.target.value)} />
+          <br/>
+          <br/>
+          <label htmlFor="password" >Password: </label>
+          <input type="password" id="password" onChange={(event) => setPassword(event.target.value)} />
+          <p id="errorMessage">{error}</p>
+          <button type="submit">Login</button>
+        </form>
     </div>
   )
 }
